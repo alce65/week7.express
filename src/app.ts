cea7adf7a4f262e138959f4a114271193448cc91
Alejandro Cerezo <alce65@hotmail.es>
2022-11-14 16:59:48 +0100

    Update express with router & controller

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,42 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { Task } from './interfaces/task';
-import importData from './mock/data.json' assert { type: 'json' };
+import { taskRouter } from './router/task.js';
 
 export const app = express();
-const data: Array<Task> = importData.tasks;
+// let data: Array<Task> = importData.tasks;
 
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
+app.use((_req, _resp, next) => {
+    console.log('Middleware');
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('API Express de tareas');
     res.end();
 });
 
-app.get('/tasks', (req, res) => {
-    res.json(data);
-    res.end();
-});
+app.use('/tasks', taskRouter);
 
-app.post('/tasks', (req, res) => {
-    const newTask = {
-        ...req.body,
-        id: data.length + 1,
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, _req: Request, resp: Response, next: NextFunction) => {
+    console.log(error.message);
+    let status = 500;
+    if (error.name === 'ValidationError') {
+        status = 406;
+    } else {
+        //
+    }
+    resp.status(status);
+    const result = {
+        status: status,
+        type: error.name,
+        error: error.message,
     };
-    data.push(newTask);
-    res.json(newTask);
-    res.end();
+    resp.json(result);
+    resp.end();
 });
