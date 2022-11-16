97d44e8e520fa6a9cf8c81413935624dec645108
Alejandro Cerezo <alce65@hotmail.es>
2022-11-16 12:30:35 +0100

    Solve sincrony problem in controller test

--- src/controllers/task.test.ts
+++ src/controllers/task.test.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { TaskFileData } from '../data/tasks.file.data';
 import { TaskController } from './tasks';
 
@@ -10,9 +10,13 @@ describe('Given TaskController', () => {
         json: jest.fn(),
         end: jest.fn(),
     };
-    test('Then ... getAll', () => {
-        taskController.getAll(req as Request, resp as unknown as Response);
+    const next = jest.fn();
+    test('Then ... getAll', async () => {
+        await taskController.getAll(
+            req as Request,
+            resp as unknown as Response,
+            next as NextFunction
+        );
         expect(resp.json).toHaveBeenCalled();
-        expect(resp.end).toHaveBeenCalled();
     });
 });
